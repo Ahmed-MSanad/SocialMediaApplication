f36feca74e666fcb88a6936bf552fe997f4c856a
Ahmed <sanada927@gmail.com>
2025-04-21 18:10:37 +0200

    front-end-angular-start

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()),
+    importProvidersFrom([
+      SweetAlert2Module.forRoot(),
+			SweetAlert2Module.forChild(),
+    ]),
+  ]
 };
